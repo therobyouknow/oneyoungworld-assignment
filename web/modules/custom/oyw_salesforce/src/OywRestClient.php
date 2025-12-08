6bc21e929036f3af7aa2ea57f4ec32c9feec3376
Rob Davis <login@therobyouknow.com>
2025-12-08 11:28:30 +0000

    decode JSON example and use in list

--- web/modules/custom/oyw_salesforce/src/OywRestClient.php
+++ web/modules/custom/oyw_salesforce/src/OywRestClient.php
@@ -41,6 +41,58 @@ class OywRestClient extends RestClient
 
     $this->apiCall("ui-api/object-info/$name/picklist-values/$record_type/$field_name");
 
+    // credit: https://gemini.google.com/share/1d8089fdb0df (due to time constraints)
+    // (used Gemini to setup json and array parsing code below)
+    // original JSON from test doc
+
+    /* start gemini */
+
+    $json_string = '{
+    "values": [
+        {
+            "attributes": null,
+            "label": "Male",
+            "validFor": [],
+            "value": "Male"
+        },
+        {
+            "attributes": null,
+            "label": "Female",
+            "validFor": [],
+            "value": "Female"
+        },
+        {
+            "attributes": null,
+            "label": "Other",
+            "validFor": [],
+            "value": "Other"
+        }
+    ]
+}';
+
+    // 2. Decode the JSON string into a PHP variable.
+    // The second argument 'true' tells json_decode to return associative arrays.
+    // If 'true' is omitted, it will return objects (of class stdClass).
+    $data_array = json_decode($json_string, true);
+
+    // Example of accessing the data:
+    //echo "The label of the first value is: " . $data_array['values'][0]['label'];
+
+    // ----------------------------------------------------
+    // 1. Initialize the new indexed array
+    // This array will hold just the 'value' fields: ["Male", "Female", "Other"]
+    $indexed_values_array = [];
+
+    // 2. Use a foreach loop to iterate over the 'values' array inside the main structure
+    // $item represents each inner array: {"attributes": null, "label": "Male", "value": "Male", ...}
+    foreach ($data_array['values'] as $item) {
+      // 3. Extract the specific 'value' and append it to the new indexed array
+      $indexed_values_array[] = $item['value'];
+    }
+
+    /* end gemini */
+
+    /*
     $options = [
       1 => 'male',
       2 => 'female',
@@ -48,8 +100,38 @@ class OywRestClient extends RestClient
       4 => 'other 2',
       5 => 'other 3',
     ];
+*/
 
-    return $options;
+    return $indexed_values_array;
+
+
+    /*
+
+{
+"values": [
+{
+"attributes": null,
+"label": "Male",
+"validFor": [],
+"value": "Male"
+},
+{
+"attributes": null,
+"label": "Female",
+"validFor": [],
+"value": "Female"
+},
+{
+"attributes": null,
+"label": "Other",
+"validFor": [],
+"value": "Other"
+}
+]
+}
+
+
+*/
   }
 
   /**
