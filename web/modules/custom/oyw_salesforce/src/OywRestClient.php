75bf4eb4a3a6b8d614f384c3d0c18537ce2ef891
Rob Davis <login@therobyouknow.com>
2025-12-08 04:20:09 +0000

    dump list values working, now need to call real salesforce

--- web/modules/custom/oyw_salesforce/src/OywRestClient.php
+++ web/modules/custom/oyw_salesforce/src/OywRestClient.php
@@ -2,18 +2,16 @@
 
 namespace Drupal\oyw_salesforce;
 
-/*
+
 use Drupal\Component\Utility\Random;
-*/
 use Drupal\salesforce\Rest\RestClient;
-/*
 use Drupal\salesforce\Rest\RestResponse;
 use Drupal\salesforce\Rest\RestResponseDescribe;
 use Drupal\salesforce\SelectQueryInterface;
 use Drupal\salesforce\SelectQueryResult;
 use Drupal\salesforce\SFID;
 use GuzzleHttp\Psr7\Response;
-*/
+
 
 /**
  * Test Salesforce REST client.
@@ -21,6 +19,13 @@ use GuzzleHttp\Psr7\Response;
  * @see tests/modules/salesforce_test_rest_client
  */
 class OywRestClient extends RestClient {
+
+  /*
+public function __construct(ClientInterface $http_client, ConfigFactoryInterface $config_factory, StateInterface $state, CacheBackendInterface $cache, Json $json, TimeInterface $time, SalesforceAuthProviderPluginManagerInterface $authManager) {
+  parent::__construct($http_client,  $config_factory,  $state,  $cache,  $json,  $time,  $authManager);
+}
+  */
+
 
   /**
    *
